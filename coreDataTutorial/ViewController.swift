91789f48ace677bfccea74ac162b7aabf413f0ee
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-10-13 12:52:04 +0200

    Initial Commit

---
+++ coreDataTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataTutorial
+//
+//  Created by Marina Beatriz Santana de Aguiar on 13.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
